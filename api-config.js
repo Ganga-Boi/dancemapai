d178df0e3c9d4deabbaa2d6c8c29f6daefd15bde
GangaBoi <132343514+Ganga-Boi@users.noreply.github.com>
2026-01-11 12:44:26 +0100

    Delete api-config.js

--- api-config.js
+++
@@ -1,7 +0,0 @@
-// DanceMap API configuration
-// JSONP via Google Apps Script (CORS-safe)
-
-window.DANCEMAP_API = {
-  search: 'https://script.google.com/macros/s/AKfycbxnZiO0bDkCrrwGJXwAvA5mWKPBoQoL-t1smx5m0A5j51gSbTOtF1MLT3qkdwbZkTX_/exec',
-  weekcount: 'https://script.google.com/macros/s/AKfycbxnZiO0bDkCrrwGJXwAvA5mWKPBoQoL-t1smx5m0A5j51gSbTOtF1MLT3qkdwbZkTX_/exec'
-};
